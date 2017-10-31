de459be75fedc1cf84ca781578c9a348a704599b
8625353 <8625353@qq.com>
2017-10-31 12:02:23 +0800

    update py

--- week3/shiyanlougithub.py
+++ week3/shiyanlougithub.py
@@ -9,8 +9,8 @@ class ShiyanlouCoursesSpider(scrapy.Spider):
         return (url_tmpl.format(i) for i in range(1,5))
 
     def parse(self,response):
-        for course in response.xpath('//li[@itemprop="owns"]/div'):
+        for course in response.xpath('//li[@itemprop="owns"]'):
             yield{
-                'name':course.xpath('.//h3/a/text()').re_first('\n        (.+)'),
-                'update_time':course.xpath('.//relative-time/@datetime').extract_first()
+                'name':course.xpath('.//div/h3/a/text()').re_first('\n        (.+)'),
+                'update_time':course.xpath('.//div/relative-time/@datetime').extract_first()
             }
