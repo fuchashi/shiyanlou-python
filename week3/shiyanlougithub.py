869472e456752a6013e94cdc09a6ff6a24ade8e9
8625353 <8625353@qq.com>
2017-10-31 11:51:59 +0800

    shiyanlougithub

---
+++ week3/shiyanlougithub.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.5
+import scrapy
+
+class ShiyanlouCoursesSpider(scrapy.Spider):
+    name='shiyanlou-courses'
+    @property
+    def start_urls(self):
+        url_tmpl='https://github.com/shiyanlou?page={}&tab=repositories'
+        return (url_tmpl.format(i) for i in range(1,5))
+
+    def parse(self,response):
+        for course in response.xpath('//li[@itemprop="owns"]/div'):
+            yield{
+                'name':course.xpath('.//h3/a/text()').re_first('\n        (.+)'),
+                'update_time':course.xpath('.//relative-time/@datetime').extract_first()
+            }
